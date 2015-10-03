644efe2106db8c8c987d3538d570e4d0897a2e6c
Harikrishna R <harik@klarsys.com>
2015-10-03 21:21:55 +0530

    Retry

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [[ "$1" == "deploy" ]]
+then
+	echo "Deploying..."
+	ssh -l harik_klarsys_com 104.154.71.241 zmote-broker/deploy-server.sh
+	exit 0
+fi
+
 cd ~/zmote-broker
 git pull
 sudo forever stop 0
